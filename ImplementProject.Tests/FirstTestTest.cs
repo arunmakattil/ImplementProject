242db979ca108a71b0b4d5efe76fe598128df9a8
Arun Ramesh <arunrameshmakattil1993@gmail.com>
2024-03-29 16:25:10 +0000

    Add project files.

---
+++ ImplementProject.Tests/FirstTestTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementProject.Tests
+{
+    public class FirstTestTest
+    {
+        [Fact]
+        public void FirstTest_SendMessage_ReturnString()
+        {
+            //Arrange
+            FirstTest pingMsg = new FirstTest();
+            //Act
+            string ressult = FirstTest.SendMessage();
+            //Assert
+
+        }
+    }
+}
---
+++ ImplementProject/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementProject
+{
+     public class FirstTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SendMessage());
+        }
+        public static string SendMessage()
+        {
+            return "asdasd";
+        }
+    }
+}
